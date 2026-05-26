f24ffefab781bb0d2e5180f042c47f5ee17a6bb4
Arijit Sinha <arijitsinha2.0@gmail.com>
2026-05-26 15:22:35 +0530

    app.get(*) replaced with app.get(/.*/)

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -20,10 +20,15 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT||5000;
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use( express.static(path.join(__dirname, "frontend", "dist")) );
+app.get( /.*/, (req, res)=>{
+    res.sendFile( path.join(__dirname, "frontend", "dist", "index.html") );
+} );
 
 const startServer = async () => {
     try{
